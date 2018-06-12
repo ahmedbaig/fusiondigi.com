ecb2e6cde852795f6b78c5ab6b3ce8b67df81d2e
Ahmed Baig <ahmebaig54@yahoo.com>
2018-06-12 07:20:10 +0500

    service workers

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const cacheName = 'fusiondigi';
+const staticAssets = [
+  './',
+  './js/compressed.js',
+  './css/*',
+  './fallback.json',
+  './images/*'
+];
+
+const cacheNames = [
+  'fusiondigi',
+  'fusiondigi-dynamics'
+];
+
+self.addEventListener('install', async function () {
+  const cache = await caches.open(cacheName);
+  cache.addAll(staticAssets);
+});
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  const url = new URL(request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
+  }
+});
+
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  return cachedResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+  const dynamicCache = await caches.open('news-dynamic');
+  try {
+    const networkResponse = await fetch(request);
+    dynamicCache.put(request, networkResponse.clone());
+    return networkResponse;
+  } catch (err) {
+    const cachedResponse = await dynamicCache.match(request);
+    return cachedResponse || await caches.match('./fallback.json');
+  }
+}
